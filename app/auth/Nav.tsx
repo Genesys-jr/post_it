d007d8bc5d42041fe7222d988f80778253fc4aaf
Amon Daniel <amon.daniel.91@gmail.com>
2023-07-22 15:34:01 +0100

    user authentication

---
+++ app/auth/Nav.tsx
@@ -0,0 +1,21 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import { signIn } from "next-auth/react";
+import Login from "@/app/auth/Login";
+import Logged from "@/app/auth/Logged";
+import Link from "next/link";
+
+export default async function Nav() {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <nav className="flex justify-between items-center py-8 ">
+      <Link href={"/"}>
+        <h1 className="font-bold text-lg">Send It.</h1>
+      </Link>
+      <ul className="flex items-center gap-6"></ul>
+      {!session?.user && <Login />}
+      {session?.user && <Logged image={session.user?.image?} />}
+    </nav>
+  );
+}
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,15 +1,20 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import prisma from '@prisma/client'
+import prisma from "../../../prisma/client";
 
+const adapter = PrismaAdapter(prisma);
 
-export default NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      }),
-    ],
-  });
+export const authOptions = {
+  // configure one or more authentication providers
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
